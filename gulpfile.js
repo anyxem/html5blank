894963d587493b0d84eaad61696b1695a8970502
Sergey Chystiakov <anyxem@gmail.com>
2016-10-04 16:31:22 +0200

    fix stylus watch

--- gulpfile.js
+++ gulpfile.js
@@ -150,7 +150,7 @@ gulp.task( "watch", [ "template", "styles", "jshint" ], function() {
 	/** Watch for autoprefix */
 	gulp.watch( [
 		"src/css/*.css",
-		"src/css/sass/**/*.scss"
+		"src/css/styl/**/*.styl"
 	], [ "styles" ] );
 
 	/** Watch for JSHint */
